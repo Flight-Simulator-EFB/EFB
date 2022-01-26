b68cc2ac3fd46084e25a858d91f4e4d17d2f0fff
Luke Else <mail@luke-else.co.uk>
2022-01-26 13:11:04 +0000

    Created NavData Model
    Create Navdata Model and add Navata controller (Currently empty)

---
+++ Models/NavdataModel.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace EFB.Models
+{
+    public class NavdataModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Frequency { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        
+
+        public NavdataModel(int id, string name, string latitude, string longitude){
+            Id = id;
+            Name = name;
+            Frequency = null;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public NavdataModel(int id, string name, int frequency, string latitude, string longitude){
+            Id = id;
+            Name = name;
+            Frequency = frequency;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public NavdataModel[] Populate(){
+            MySqlConnection db = new MySqlConnection("root:XXXXXXX@XXX.XXX.XXX.XXX:3306/EFB");
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
